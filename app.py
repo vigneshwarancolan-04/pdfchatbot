28c5da2dc6d88d779b721540a47ec33c3ca8698a
vigneshwarancolan-04 <vigneshwarancolan@gmail.com>
2025-09-17 13:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,5 +214,5 @@ def chat(session_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8080"))
+    port = int(os.environ.get("PORT", "8181"))
     app.run(host="0.0.0.0", port=port)
